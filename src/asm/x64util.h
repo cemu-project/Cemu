ad695b1e95fdd8840955a1370c4681994bc1feb5
Zopolis4 <creatorsmithmdt@gmail.com>
2022-08-26 15:40:19 +1000

    break build

--- src/asm/x64util.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-extern "C" uint64 ATTR_MS_ABI udiv128(uint64 low, uint64 hi, uint64 divisor, uint64 *remainder);
-extern "C" void recompiler_fres();
-extern "C" void recompiler_frsqrte();
