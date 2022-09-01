d156ad0a0fc043356d2983a788849e69bf256999
Tom Lally <tomlally@protonmail.com>
2022-09-01 17:13:14 +0100

    Cleanup

--- src/util/SystemInfo/SystemInfo.h
+++ src/util/SystemInfo/SystemInfo.h
@@ -10,9 +10,8 @@ struct ProcessorTime
 	static double Compare(ProcessorTime &last, ProcessorTime &now);
 };
 
+uint32_t GetProcessorCount();
+uint64_t QueryRamUsage();
 void QueryProcTime(uint64_t &out_now, uint64_t &out_user, uint64_t &out_kernel);
 void QueryProcTime(ProcessorTime &out);
 void QueryCoreTimes(uint32_t count, ProcessorTime out[]);
-
-uint32_t GetProcessorCount();
-uint64_t QueryRamUsage();
