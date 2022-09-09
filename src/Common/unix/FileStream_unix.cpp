dbf582b6cc770de9beb3c21de48f4a3f55912f69
goeiecool9999 <>
2022-09-09 15:20:06 +0200

    fix for when file open is successful but the path isn't a file

--- src/Common/unix/FileStream_unix.cpp
+++ src/Common/unix/FileStream_unix.cpp
@@ -198,6 +198,9 @@ FileStream::FileStream(const fs::path& path, bool isOpen, bool isWriteable)
 		m_fileStream.open(path, std::ios_base::in | std::ios_base::out | std::ios_base::binary | std::ios_base::trunc);
 		m_isValid = m_fileStream.is_open();
 	}
+	m_isValid &= !is_directory(path);
+	if(!m_isValid)
+		m_fileStream.close();
 }
 
 void FileStream::SyncReadWriteSeek(bool nextOpIsWrite)
