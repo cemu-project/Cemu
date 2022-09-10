4c6f22ba3991a594c766342412c169de31724709
Fs00 <francescosaltori@gmail.com>
2022-09-10 13:55:37 +0200

    Simplify a few window names

--- src/gui/debugger/SymbolWindow.cpp
+++ src/gui/debugger/SymbolWindow.cpp
@@ -13,7 +13,7 @@ enum ItemColumns
 };
 
 SymbolWindow::SymbolWindow(DebuggerWindow2& parent, const wxPoint& main_position, const wxSize& main_size)
-	: wxFrame(&parent, wxID_ANY, _("Symbol Window"), wxDefaultPosition, wxSize(600, 250), wxSYSTEM_MENU | wxCAPTION | wxCLIP_CHILDREN | wxRESIZE_BORDER | wxFRAME_FLOAT_ON_PARENT)
+	: wxFrame(&parent, wxID_ANY, _("Symbols"), wxDefaultPosition, wxSize(600, 250), wxSYSTEM_MENU | wxCAPTION | wxCLIP_CHILDREN | wxRESIZE_BORDER | wxFRAME_FLOAT_ON_PARENT)
 {
 	this->wxWindowBase::SetBackgroundColour(*wxWHITE);
 
