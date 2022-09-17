49a0c6d495a3a402b000051595a50bc07ad8e8f6
goeiecool9999 <>
2022-09-17 16:01:02 +0200

    consistent use of tabs

--- src/Common/ExceptionHandler/ExceptionHandler_posix.cpp
+++ src/Common/ExceptionHandler/ExceptionHandler_posix.cpp
@@ -18,15 +18,15 @@ void handlerDumpingSignal(int sig)
 		printf("Unknown core dumping signal!\n");
 	}
 
-    void *array[32];
-    size_t size;
+	void *array[32];
+	size_t size;
 
-    // get void*'s for all entries on the stack
-    size = backtrace(array, 32);
+	// get void*'s for all entries on the stack
+	size = backtrace(array, 32);
 
-    // print out all the frames to stderr
-    fprintf(stderr, "Error: signal %d:\n", sig);
-    backtrace_symbols_fd(array, size, STDERR_FILENO);
+	// print out all the frames to stderr
+	fprintf(stderr, "Error: signal %d:\n", sig);
+	backtrace_symbols_fd(array, size, STDERR_FILENO);
 
 	if (GetConfig().crash_dump == CrashDump::Full)
 	{
@@ -36,19 +36,19 @@ void handlerDumpingSignal(int sig)
 		return;
 	}
 	// Exit process ignoring all issues.
-    _Exit(1);
+	_Exit(1);
 }
 
 void handler_SIGINT(int sig)
 {
-    /*
-     * Received when pressing CTRL + C in a console
-     * Ideally should be exiting cleanly after saving settings but currently
-     * there's no clean exit pathway (at least on linux) and exiting the app
-     * by any mean ends up with a SIGABRT from the standard library destroying
-     * threads.
-     */
-    _Exit(0);
+	/*
+	 * Received when pressing CTRL + C in a console
+	 * Ideally should be exiting cleanly after saving settings but currently
+	 * there's no clean exit pathway (at least on linux) and exiting the app
+	 * by any mean ends up with a SIGABRT from the standard library destroying
+	 * threads.
+	 */
+	_Exit(0);
 }
 
 void ExceptionHandler_init()
