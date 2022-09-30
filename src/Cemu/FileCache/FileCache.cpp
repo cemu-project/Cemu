09cd94f32862ac40669e966f37c6361e532bfeb3
ssimco <ssimco-github@proton.me>
2022-09-30 12:30:11 +0300

    Revert "Fix FileCache::Create failing to create file if folder doesn't exist"
    This reverts commit e3d99af9682eabefec07fb3d119c65b95a94c4ac.

--- src/Cemu/FileCache/FileCache.cpp
+++ src/Cemu/FileCache/FileCache.cpp
@@ -89,12 +89,7 @@ private:
 
 FileCache* FileCache::Create(wzstring_view path, uint32 extraVersion)
 {
-	std::wstring wpath(path);
-	fs::path pathParent = fs::path(wpath).parent_path();
-	if (!fs::exists(pathParent))
-		fs::create_directories(pathParent);
-
-	FileStream* fs = FileStream::createFile(wpath.c_str());
+	FileStream* fs = FileStream::createFile(std::wstring(path).c_str());
 	if (!fs)
 	{
 		forceLog_printf("Failed to create cache file \"%ls\"", path);
