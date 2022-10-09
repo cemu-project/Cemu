2f02adf29692fdeeae7553ffcf38e523bd4e875e
Tillsunset <larasebastian001@gmail.com>
2022-10-09 01:28:11 -0500

    whitespace

--- src/Cafe/OS/libs/h264_avc/H264Dec.cpp
+++ src/Cafe/OS/libs/h264_avc/H264Dec.cpp
@@ -194,7 +194,7 @@ namespace H264
 #ifdef _WIN32
 			return _aligned_malloc(size, alignment);
 #else
-            size += alignment - (size%alignment);
+			size += alignment - (size % alignment);
 			return aligned_alloc(alignment, size);
 #endif
 		}
