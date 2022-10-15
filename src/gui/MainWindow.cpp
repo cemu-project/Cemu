674fcff07642104c688b86babe6bdaa65ad19a49
goeiecool9999 <7033575+goeiecool9999@users.noreply.github.com>
2022-10-15 13:58:58 +0200

    fix a crash where wxwidgets tries to free a stack allocated object.

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -759,11 +759,12 @@ void MainWindow::OpenSettings()
 	auto& config = GetConfig();
 	const auto language = config.language;
 
-	GeneralSettings2 frame(this, m_game_launched);
-	frame.ShowModal();
-	const bool paths_modified = frame.ShouldReloadGamelist();
-	const bool mlc_modified = frame.MLCModified();
-	frame.Destroy();
+	GeneralSettings2* frame = new GeneralSettings2(this, m_game_launched);
+	frame->ShowModal();
+	const bool paths_modified = frame->ShouldReloadGamelist();
+	const bool mlc_modified = frame->MLCModified();
+	frame->Destroy();
+	frame = nullptr;
 
 	if (paths_modified)
 		m_game_list->ReloadGameEntries(false);
