665a34e51847061e31398a04506dcf2f53dd15f9
Exzap <13877693+Exzap@users.noreply.github.com>
2022-10-17 13:25:49 +0200

    Linux: Always use libpng from system (#381)
    * Always use system libpng on Linux
    * Remove dependency on boost-crc in DSU (reuse existing implementation)

--- src/util/crypto/crc32.h
+++ src/util/crypto/crc32.h
@@ -1,3 +1,8 @@
 #pragma once
 
 unsigned int crc32_calc(unsigned int c, const void* data, int length);
+
+inline unsigned int crc32_calc(const void* data, int length)
+{
+	return crc32_calc(0, data, length);
+}
