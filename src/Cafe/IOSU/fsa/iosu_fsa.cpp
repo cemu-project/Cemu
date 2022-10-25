7c06a8e04d845c3f99024705d4e02c38ad44142e
GaryOderNichts <12049776+GaryOderNichts@users.noreply.github.com>
2022-10-26 00:09:43 +0200

    FSA: Add missing path translations

--- src/Cafe/IOSU/fsa/iosu_fsa.cpp
+++ src/Cafe/IOSU/fsa/iosu_fsa.cpp
@@ -328,26 +328,26 @@ namespace iosu
 
 		FSStatus FSAProcessCmd_remove(FSAClient* client, FSAIpcCommand* cmd)
 		{
-			char* path = (char*)cmd->cmdRemove.path;
+			std::string path = __FSATranslatePath(client, (char*)cmd->cmdRemove.path);
 			sint32 fscStatus = FSC_STATUS_FILE_NOT_FOUND;
-			fsc_remove(path, &fscStatus);
+			fsc_remove(path.c_str(), &fscStatus);
 			return FSA_convertFSCtoFSStatus(fscStatus);
 		}
 
 		FSStatus FSAProcessCmd_makeDir(FSAClient* client, FSAIpcCommand* cmd)
 		{
-			char* path = (char*)cmd->cmdMakeDir.path;
+			std::string path = __FSATranslatePath(client, (char*)cmd->cmdMakeDir.path);
 			sint32 fscStatus = FSC_STATUS_FILE_NOT_FOUND;
-			fsc_createDir(path, &fscStatus);
+			fsc_createDir(path.c_str(), &fscStatus);
 			return FSA_convertFSCtoFSStatus(fscStatus);
 		}
 
 		FSStatus FSAProcessCmd_rename(FSAClient* client, FSAIpcCommand* cmd)
 		{
-			char* srcPath = (char*)cmd->cmdRename.srcPath;
-			char* dstPath = (char*)cmd->cmdRename.dstPath;
+			std::string srcPath = __FSATranslatePath(client, (char*)cmd->cmdRename.srcPath);
+			std::string dstPath = __FSATranslatePath(client, (char*)cmd->cmdRename.dstPath);
 			sint32 fscStatus = FSC_STATUS_FILE_NOT_FOUND;
-			fsc_rename(srcPath, dstPath, &fscStatus);
+			fsc_rename(srcPath.c_str(), dstPath.c_str(), &fscStatus);
 			return FSA_convertFSCtoFSStatus(fscStatus);
 		}
 
