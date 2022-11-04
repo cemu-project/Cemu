7fb8731f15fb4c2c0b36f6fbfe23c6bb416f3ef9
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2022-11-04 11:31:30 -0500

    Add license to about modal

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -1869,6 +1869,57 @@ public:
 			lineSizer->Add(new wxStaticText(parent, -1, ")"), 0);
 			sizer->Add(lineSizer);
 		}
+		// GameMode
+		{
+			wxSizer *lineSizer = new wxBoxSizer(wxHORIZONTAL);
+			lineSizer->Add(new wxStaticText(parent, -1, "Feral GameMode ("), 0);
+			lineSizer->Add(new wxHyperlinkCtrl(parent, -1, "Source", "https://github.com/FeralInteractive/gamemode"), 0);
+			lineSizer->Add(new wxStaticText(parent, -1, "  "), 0);
+			wxHyperlinkCtrl *noticeLink = new wxHyperlinkCtrl(parent, -1, "NOTICE", "");
+			noticeLink->Bind(wxEVT_LEFT_DOWN, [](wxMouseEvent &event)
+											 {
+					fs::path tempPath = fs::temp_directory_path();
+					tempPath.append("NOTICE_IH264.txt");
+					FileStream* fs = FileStream::createFile2(tempPath);
+					if (!fs)
+						return;
+					fs->writeString(
+							"/*\n"
+							"\n"
+							"Copyright (c) 2017-2019, Feral Interactive\n"
+							"All rights reserved.\n"
+							"\n"
+							"Redistribution and use in source and binary forms, with or without\n"
+							"modification, are permitted provided that the following conditions are met:\n"
+							"\n"
+							" * Redistributions of source code must retain the above copyright notice,\n"
+							"   this list of conditions and the following disclaimer.\n"
+							" * Redistributions in binary form must reproduce the above copyright\n"
+							"   notice, this list of conditions and the following disclaimer in the\n"
+							"   documentation and/or other materials provided with the distribution.\n"
+							" * Neither the name of Feral Interactive nor the names of its contributors\n"
+							"   may be used to endorse or promote products derived from this software\n"
+							"   without specific prior written permission.\n"
+							"\n"
+							"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n"
+							"AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n"
+							"IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE\n"
+							"ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE\n"
+							"LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR\n"
+							"CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF\n"
+							"SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS\n"
+							"INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN\n"
+							"CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)\n"
+							"ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE\n"
+							"POSSIBILITY OF SUCH DAMAGE.\n"
+							"\n"
+							" */");
+					delete fs;
+					wxLaunchDefaultBrowser(wxHelper::FromUtf8(fmt::format("file:{}", _pathToUtf8(tempPath)))); });
+			lineSizer->Add(noticeLink, 0);
+			lineSizer->Add(new wxStaticText(parent, -1, ")"), 0);
+			sizer->Add(lineSizer);
+		}
 	}
 
 	void AddThanks(wxWindow* parent, wxSizer* sizer)
