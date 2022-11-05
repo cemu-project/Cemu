14d82ae4a50c08c01308f92b00fe08181e4a8ffe
Exzap <13877693+Exzap@users.noreply.github.com>
2022-11-05 08:27:30 +0100

    PPCRec: Move analyzer file + move some funcs to IMLInstruction

--- src/Cafe/HW/Espresso/Recompiler/IML/IML.h
+++ src/Cafe/HW/Espresso/Recompiler/IML/IML.h
@@ -1,3 +1,18 @@
+#pragma once 
+
+#include "IMLInstruction.h"
+#include "IMLSegment.h"
+
+// analyzer
+struct PPCRecCRTracking_t
+{
+	uint32 readCRBits;
+	uint32 writtenCRBits;
+};
+
+bool IMLAnalyzer_IsTightFiniteLoop(IMLSegment* imlSegment);
+bool IMLAnalyzer_CanTypeWriteCR(IMLInstruction* imlInstruction);
+void IMLAnalyzer_GetCRTracking(IMLInstruction* imlInstruction, PPCRecCRTracking_t* crTracking);
 
 // debug
 void IMLDebug_DumpSegment(struct IMLSegment* imlSegment, sint32 segmentIndex, bool printLivenessRangeInfo = false);
