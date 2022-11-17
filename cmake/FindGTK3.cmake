8329b8780c48bab1592bc9a609903b18a11ddf5f
goeiecool9999 <7033575+goeiecool9999@users.noreply.github.com>
2022-11-17 22:58:00 +0100

    Update cmake/FindGTK3.cmake
    Co-authored-by: Andrea Pappacoda <andrea@pappacoda.it>

--- cmake/FindGTK3.cmake
+++ cmake/FindGTK3.cmake
@@ -5,17 +5,12 @@ include(FindPackageHandleStandardArgs)
 
 find_package(PkgConfig REQUIRED)
 if (PKG_CONFIG_FOUND)
-    pkg_search_module(GTK3 REQUIRED gtk+-3.0)
+    pkg_search_module(GTK3 IMPORTED_TARGET gtk+-3.0)
     if (GTK3_FOUND)
-        add_library(GTK3::gtk IMPORTED INTERFACE)
-        target_link_libraries(GTK3::gtk INTERFACE ${GTK3_LIBRARIES})
-        target_link_directories(GTK3::gtk INTERFACE ${GTK3_LIBRARY_DIRS})
-        target_include_directories(GTK3::gtk INTERFACE ${GTK3_INCLUDE_DIRS})
+        add_library(GTK3::gtk ALIAS PkgConfig::GTK3)
     endif()
     find_package_handle_standard_args(GTK3
-            REQUIRED_VARS
-            GTK3_LINK_LIBRARIES
-            GTK3_FOUND
+            REQUIRED_VARS GTK3_LINK_LIBRARIES
             VERSION_VAR GTK3_VERSION
     )
 endif()
