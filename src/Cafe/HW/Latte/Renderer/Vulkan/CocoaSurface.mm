995fa581736148ef6eb76643c6458a8b1ba2687d
goeiecool9999 <7033575+goeiecool9999@users.noreply.github.com>
2022-11-20 15:18:14 +0100

    update macOS scaling code according to MVK example

--- src/Cafe/HW/Latte/Renderer/Vulkan/CocoaSurface.mm
+++ src/Cafe/HW/Latte/Renderer/Vulkan/CocoaSurface.mm
@@ -13,8 +13,23 @@
 
 +(Class) layerClass { return [CAMetalLayer class]; }
 
--(CALayer*) makeBackingLayer { return [self.class.layerClass layer]; }
+// copied from https://github.com/KhronosGroup/MoltenVK/blob/master/Demos/Cube/macOS/DemoViewController.m
 
+-(CALayer*) makeBackingLayer
+{
+	CALayer* layer = [self.class.layerClass layer];
+	CGSize viewScale = [self convertSizeToBacking: CGSizeMake(1.0, 1.0)];
+	layer.contentsScale = MIN(viewScale.width, viewScale.height);
+	return layer;
+}
+
+-(BOOL) layer: (CALayer *)layer shouldInheritContentsScale: (CGFloat)newScale fromWindow: (NSWindow *)window
+{
+	if (newScale == layer.contentsScale) { return NO; }
+
+	layer.contentsScale = newScale;
+	return YES;
+}
 @end
 
 VkSurfaceKHR CreateCocoaSurface(VkInstance instance, void* handle)
