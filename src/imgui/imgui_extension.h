79f2780edd6d2e78fe2546c1e7d75393dec4d2d4
goeiecool9999 <7033575+goeiecool9999@users.noreply.github.com>
2022-11-30 18:39:47 +0100

    make main/pad window distinct so they can have different sizes

--- src/imgui/imgui_extension.h
+++ src/imgui/imgui_extension.h
@@ -9,6 +9,8 @@ inline ImVec2 operator-(const ImVec2& l, const ImVec2& r) { return{ l.x - r.x, l
 inline bool operator<(const ImVec2& lhs, const ImVec2& rhs) { return lhs.x < rhs.x; }
 inline bool operator>=(const ImVec2& lhs, const ImVec2& rhs) { return lhs.x >= rhs.x; }
 
+bool ImGui_BeginPadDistinct(const char* name, bool* p_open, ImGuiWindowFlags flags, bool pad);
+
 void ImGui_PrecacheFonts();
 ImFont* ImGui_GetFont(float size);
 void ImGui_UpdateWindowInformation(bool mainWindow);
