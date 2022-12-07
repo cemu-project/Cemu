d4aa05723f73aeb10a06579c8d1f3c7343d8e8ce
jn64 <23169302+jn64@users.noreply.github.com>
2022-12-07 20:21:53 +0800

    Reset desktop file in AppImage
    AppImage build fails because it can't find env
    
    GDK_BACKEND=x11 is still exported in linuxdeploy-plugin-gtk.sh

--- dist/linux/appimage.sh
+++ dist/linux/appimage.sh
@@ -15,6 +15,8 @@ if [[ ! -e /usr/lib/x86_64-linux-gnu ]]; then
 	sed -i 's#lib\/x86_64-linux-gnu#lib64#g' linuxdeploy-plugin-gtk.sh
 fi
 
+sed -i '/^Exec=/ c Exec=Cemu' dist/linux/info.cemu.Cemu.desktop
+
 mkdir -p AppDir/usr/bin
 cp dist/linux/{info.cemu.Cemu.desktop,info.cemu.Cemu.png} AppDir/
 
