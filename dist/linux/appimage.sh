ba2314edbe33fce3a52943f3f82e0a81bd5a91b3
jn64 <23169302+jn64@users.noreply.github.com>
2022-12-08 00:52:00 +0800

    Quote all expansions, prefer bash syntax according to the shebang
    Passes shellcheck

--- dist/linux/appimage.sh
+++ dist/linux/appimage.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [ -z "${GITHUB_WORKSPACE}" ]; then
+if [[ -z "${GITHUB_WORKSPACE}" ]]; then
     export GITHUB_WORKSPACE="."
 fi
-  
+
 curl -sSfLO "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
 chmod a+x linuxdeploy*.AppImage
-curl -sSfL https://github.com$(curl https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous | grep "mkappimage-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2) -o mkappimage.AppImage
+curl -sSfL https://github.com"$(curl https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous | grep "mkappimage-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)" -o mkappimage.AppImage
 chmod a+x mkappimage.AppImage
 curl -sSfLO "https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gtk/master/linuxdeploy-plugin-gtk.sh"
 chmod a+x linuxdeploy-plugin-gtk.sh
 
 if [[ ! -e /usr/lib/x86_64-linux-gnu ]]; then
-	sed -i 's#lib\/x86_64-linux-gnu#lib64#g' linuxdeploy-plugin-gtk.sh
+    sed -i 's#lib\/x86_64-linux-gnu#lib64#g' linuxdeploy-plugin-gtk.sh
 fi
 
 mkdir -p AppDir/usr/bin
@@ -29,23 +29,23 @@ mv AppDir/usr/share/Cemu/Cemu AppDir/usr/bin/
 chmod +x AppDir/usr/bin/Cemu
 
 export UPD_INFO="gh-releases-zsync|cemu-project|Cemu|ci|Cemu.AppImage.zsync"
-./linuxdeploy-x86_64.AppImage --appimage-extract-and-run\
-  --appdir="$GITHUB_WORKSPACE"/AppDir/          	\
-  -d "$GITHUB_WORKSPACE"/AppDir/info.cemu.Cemu.desktop  \
-  -i "$GITHUB_WORKSPACE"/AppDir/info.cemu.Cemu.png      \
-  -e "$GITHUB_WORKSPACE"/AppDir/usr/bin/Cemu		\
+./linuxdeploy-x86_64.AppImage --appimage-extract-and-run \
+  --appdir="${GITHUB_WORKSPACE}"/AppDir/ \
+  -d "${GITHUB_WORKSPACE}"/AppDir/info.cemu.Cemu.desktop \
+  -i "${GITHUB_WORKSPACE}"/AppDir/info.cemu.Cemu.png \
+  -e "${GITHUB_WORKSPACE}"/AppDir/usr/bin/Cemu \
   --plugin gtk
 
-GITVERSION=$(git rev-parse --short HEAD) 
-echo $GITVERSION
-if [[ -z ${GITVERSION} ]]; then
-	GITVERSION=experimental
+GITVERSION="$(git rev-parse --short HEAD)"
+echo "${GITVERSION}"
+if [[ -z "${GITVERSION}" ]]; then
+    GITVERSION=experimental
 fi
 
 echo "Cemu Version Cemu-${GITVERSION}"
 rm AppDir/usr/lib/libwayland-client.so.0
 echo "export LC_ALL=C" >> AppDir/apprun-hooks/linuxdeploy-plugin-gtk.sh
-VERSION=${GITVERSION} ./mkappimage.AppImage --appimage-extract-and-run "$GITHUB_WORKSPACE"/AppDir
+VERSION="${GITVERSION}" ./mkappimage.AppImage --appimage-extract-and-run "${GITHUB_WORKSPACE}"/AppDir
 
-mkdir -p "$GITHUB_WORKSPACE"/artifacts/ 
-mv Cemu-${GITVERSION}-x86_64.AppImage "$GITHUB_WORKSPACE"/artifacts/ 
+mkdir -p "${GITHUB_WORKSPACE}"/artifacts/
+mv Cemu-"${GITVERSION}"-x86_64.AppImage "${GITHUB_WORKSPACE}"/artifacts/
