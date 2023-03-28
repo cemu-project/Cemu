998f70b4f317977eb8b0f180691095941c1fe41a
JP-Carr <60516230+why-keith@users.noreply.github.com>
2023-03-28 23:45:07 +0100

    Fixed multi-line logs

--- src/Cafe/HW/Latte/Renderer/OpenGL/RendererShaderGL.cpp
+++ src/Cafe/HW/Latte/Renderer/OpenGL/RendererShaderGL.cpp
@@ -192,7 +192,7 @@ bool RendererShaderGL::WaitForCompiled()
 			tempLength = sizeof(infoLog) - 1;
 			glGetShaderInfoLog(m_shader_object, std::min(infoLogLength, tempLength), (GLsizei*)&tempLength, (GLcharARB*)infoLog);
 			infoLog[tempLength] = '\0';
-			forceLog_printf("Compile error in shader. Log:", infoLog);
+			cemuLog_log(LogType::Force, "Compile error in shader. Log:");
 			cemuLog_log(LogType::Force, infoLog);
 		}
 		if (m_shader_object != 0)
@@ -212,7 +212,7 @@ bool RendererShaderGL::WaitForCompiled()
 			tempLength = sizeof(infoLog) - 1;
 			glGetProgramInfoLog(m_program, std::min(infoLogLength, tempLength), (GLsizei*)&tempLength, (GLcharARB*)infoLog);
 			infoLog[tempLength] = '\0';
-			forceLog_printf("Link error in shader. Log:", infoLog);
+			cemuLog_log(LogType::Force, "Link error in shader. Log:");
 			cemuLog_log(LogType::Force, infoLog);
 		}
 		m_isCompiled = true;
