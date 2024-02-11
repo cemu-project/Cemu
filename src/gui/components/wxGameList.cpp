5eeb13cd4f27d290337e99b90a293889e6049695
capitalistspz <keipitalists@proton.me>
2024-02-11 03:20:51 +0000

    Add copy title ID and copy title name

--- src/gui/components/wxGameList.cpp
+++ src/gui/components/wxGameList.cpp
@@ -17,6 +17,8 @@
 #include <wx/imagpng.h>
 #include <wx/string.h>
 #include <wx/utils.h>
+#include <wx/clipbrd.h>
+
 
 #include <boost/algorithm/string.hpp>
 #include <boost/tokenizer.hpp>
@@ -546,7 +548,11 @@ enum ContextMenuEntries
 	kContextMenuStyleList,
 	kContextMenuStyleIcon,
 	kContextMenuStyleIconSmall,
-    kContextMenuCreateShortcut
+
+    kContextMenuCreateShortcut,
+
+    kContextMenuCopyTitleName,
+    kContextMenuCopyTitleId
 };
 void wxGameList::OnContextMenu(wxContextMenuEvent& event)
 {
@@ -591,6 +597,9 @@ void wxGameList::OnContextMenu(wxContextMenuEvent& event)
 #if BOOST_OS_LINUX || BOOST_OS_WINDOWS
             menu.Append(kContextMenuCreateShortcut, _("&Create shortcut"));
 #endif
+            menu.AppendSeparator();
+            menu.Append(kContextMenuCopyTitleName, _("&Copy Title Name"));
+            menu.Append(kContextMenuCopyTitleId, _("&Copy Title ID"));
 			menu.AppendSeparator();
 		}
 	}
@@ -711,10 +720,30 @@ void wxGameList::OnContextMenuSelected(wxCommandEvent& event)
 				break;
 			}
             case kContextMenuCreateShortcut:
+            {
 #if BOOST_OS_LINUX || BOOST_OS_WINDOWS
-                 CreateShortcut(gameInfo);
+                CreateShortcut(gameInfo);
 #endif
                 break;
+            }
+            case kContextMenuCopyTitleName:
+            {
+                if (wxTheClipboard->Open())
+                {
+                    wxTheClipboard->SetData(new wxTextDataObject(gameInfo.GetTitleName()));
+                    wxTheClipboard->Close();
+                }
+                break;
+            }
+            case kContextMenuCopyTitleId:
+            {
+                if (wxTheClipboard->Open())
+                {
+                    wxTheClipboard->SetData(new wxTextDataObject(fmt::format("{:016x}", title_id)));
+                    wxTheClipboard->Close();
+                }
+                break;
+            }
 			}
 		}
 	}
