2a5ad4e10556e4abb7ea067c51c8f88e5d2a4cad
goeiecool9999 <7033575+goeiecool9999@users.noreply.github.com>
2024-03-11 00:53:18 +0100

    edit comment

--- src/Cafe/HW/Latte/Renderer/OpenGL/OpenGLRenderer.cpp
+++ src/Cafe/HW/Latte/Renderer/OpenGL/OpenGLRenderer.cpp
@@ -248,7 +248,6 @@ void LoadOpenGLImports()
 #if BOOST_OS_LINUX
 // dummy function for all code that is statically linked with cemu and attempts to use eglSwapInterval
 // used to suppress wxWidgets calls to eglSwapInterval
-// Real eglSwapInterval is a function pointer defined as eglSwapIntervalCemu
 extern "C"
 EGLAPI EGLBoolean EGLAPIENTRY eglSwapInterval(EGLDisplay dpy, EGLint interval)
 {
