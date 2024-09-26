00ff45d66b8d6b89d294804c8e47b9d5f626c4da
capitalistspz <keipitalists@proton.me>
2024-09-26 02:50:02 +0100

    PairingDialog: Implement 'pairing' for bluez, reformat file

--- src/input/api/Wiimote/l2cap/L2CapWiimote.h
+++ src/input/api/Wiimote/l2cap/L2CapWiimote.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <api/Wiimote/WiimoteDevice.h>
+#include <input/api/Wiimote/WiimoteDevice.h>
 #include <bluetooth/bluetooth.h>
 
 class L2CapWiimote : public WiimoteDevice
@@ -12,7 +12,7 @@ class L2CapWiimote : public WiimoteDevice
 	std::optional<std::vector<uint8>> read_data() override;
 	bool operator==(WiimoteDevice& o) const override;
 
-	static void AddCandidateAddresses(const std::vector<bdaddr_t>& addrs);
+	static void AddCandidateAddress(bdaddr_t addr);
 	static std::vector<WiimoteDevicePtr> get_devices();
   private:
 	int m_recvFd;
