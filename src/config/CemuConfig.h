eb1945e8265e494666c8e0f770ead069566de044
Joshua de Reeper <joshua@dereeper.co.nz>
2025-01-30 12:31:43 +0100

    review changes

--- src/config/CemuConfig.h
+++ src/config/CemuConfig.h
@@ -479,7 +479,7 @@ struct CemuConfig
 	// audio
 	sint32 audio_api = 0;
 	sint32 audio_delay = 2;
-	AudioChannels tv_channels = kStereo, pad_channels = kStereo, input_channels = kMono, portal_channels = kMono;
+	AudioChannels tv_channels = kStereo, pad_channels = kStereo, input_channels = kMono;
 	sint32 tv_volume = 50, pad_volume = 0, input_volume = 50, portal_volume = 50;
 	std::wstring tv_device{ L"default" }, pad_device, input_device, portal_device;
 
