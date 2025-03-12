c3419d558976e6ce1eb654a2914e069f9565d0d6
capitalistspz <keipitalists@proton.me>
2025-03-12 00:07:01 +0000

    More accurate descriptions for descriptors

--- src/input/api/Wiimote/l2cap/L2CapWiimote.cpp
+++ src/input/api/Wiimote/l2cap/L2CapWiimote.cpp
@@ -24,14 +24,14 @@ static bool AttemptSetNonBlock(int sockFd)
 }
 
 L2CapWiimote::L2CapWiimote(int recvFd, int sendFd, bdaddr_t addr)
-	: m_recvFd(recvFd), m_sendFd(sendFd), m_addr(addr)
+	: m_controlFd(recvFd), m_dataFd(sendFd), m_addr(addr)
 {
 }
 
 L2CapWiimote::~L2CapWiimote()
 {
-	close(m_recvFd);
-	close(m_sendFd);
+	close(m_controlFd);
+	close(m_dataFd);
 	const auto& b = m_addr.b;
 	cemuLog_logDebug(LogType::Force, "Wiimote at {:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x} disconnected", b[5], b[4], b[3], b[2], b[1], b[0]);
 
@@ -61,9 +61,9 @@ std::vector<WiimoteDevicePtr> L2CapWiimote::get_devices()
 	std::vector<WiimoteDevicePtr> outDevices;
 	for (const auto& addr : unconnected)
 	{
-		// Socket for sending data to controller, PSM 0x11
-		auto sendFd = socket(PF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
-		if (sendFd < 0)
+		// Control socket, PSM 0x11, needs to be open for the data socket to be opened
+		auto controlFd = socket(PF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
+		if (controlFd < 0)
 		{
 			cemuLog_logDebug(LogType::Force, "Failed to open send socket: {}", strerror(errno));
 			continue;
@@ -74,21 +74,21 @@ std::vector<WiimoteDevicePtr> L2CapWiimote::get_devices()
 		sendAddr.l2_psm = htobs(0x11);
 		sendAddr.l2_bdaddr = addr;
 
-		if (!AttemptConnect(sendFd, sendAddr) || !AttemptSetNonBlock(sendFd))
+		if (!AttemptConnect(controlFd, sendAddr) || !AttemptSetNonBlock(controlFd))
 		{
 			const auto& b = addr.b;
 			cemuLog_logDebug(LogType::Force, "Failed to connect send socket to '{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}': {}",
 							 b[5], b[4], b[3], b[2], b[1], b[0], strerror(errno));
-			close(sendFd);
+			close(controlFd);
 			continue;
 		}
 
-		// Socket for receiving data from controller, PSM 0x13
-		auto recvFd = socket(PF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
-		if (recvFd < 0)
+		// Socket for sending and receiving data from controller, PSM 0x13
+		auto dataFd = socket(PF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
+		if (dataFd < 0)
 		{
 			cemuLog_logDebug(LogType::Force, "Failed to open recv socket: {}", strerror(errno));
-			close(sendFd);
+			close(controlFd);
 			continue;
 		}
 		sockaddr_l2 recvAddr{};
@@ -96,16 +96,16 @@ std::vector<WiimoteDevicePtr> L2CapWiimote::get_devices()
 		recvAddr.l2_psm = htobs(0x13);
 		recvAddr.l2_bdaddr = addr;
 
-		if (!AttemptConnect(recvFd, recvAddr) || !AttemptSetNonBlock(recvFd))
+		if (!AttemptConnect(dataFd, recvAddr) || !AttemptSetNonBlock(dataFd))
 		{
 			const auto& b = addr.b;
 			cemuLog_logDebug(LogType::Force, "Failed to connect recv socket to '{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}': {}",
 							 b[5], b[4], b[3], b[2], b[1], b[0], strerror(errno));
-			close(sendFd);
-			close(recvFd);
+			close(controlFd);
+			close(dataFd);
 			continue;
 		}
-		outDevices.emplace_back(std::make_shared<L2CapWiimote>(sendFd, recvFd, addr));
+		outDevices.emplace_back(std::make_shared<L2CapWiimote>(controlFd, dataFd, addr));
 
 		s_addressMutex.lock();
 		s_addresses[addr] = true;
@@ -123,13 +123,13 @@ bool L2CapWiimote::write_data(const std::vector<uint8>& data)
 	buffer[0] = 0xA2;
 	std::memcpy(buffer + 1, data.data(), size);
 	const auto outSize = size + 1;
-	return send(m_sendFd, buffer, outSize, 0) == outSize;
+	return send(m_dataFd, buffer, outSize, 0) == outSize;
 }
 
 std::optional<std::vector<uint8>> L2CapWiimote::read_data()
 {
 	uint8 buffer[23];
-	const auto nBytes = recv(m_sendFd, buffer, 23, 0);
+	const auto nBytes = recv(m_dataFd, buffer, 23, 0);
 
 	if (nBytes < 0 && errno == EWOULDBLOCK)
 		return std::vector<uint8>{};
