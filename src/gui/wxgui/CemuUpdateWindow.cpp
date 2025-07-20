73b1dc1663c19b18c6ba88bcde824a0e6fdd5905
Exzap <13877693+Exzap@users.noreply.github.com>
2025-07-20 23:10:13 +0200

    UI: Add architecture to auto-updater check

--- src/gui/wxgui/CemuUpdateWindow.cpp
+++ src/gui/wxgui/CemuUpdateWindow.cpp
@@ -111,14 +111,22 @@ bool CemuUpdateWindow::QueryUpdateInfo(std::string& downloadUrlOut, std::string&
 	auto* curl = curl_easy_init();
 	urlStr.append(_curlUrlEscape(curl, BUILD_VERSION_STRING));
 #if BOOST_OS_LINUX
-	urlStr.append("&platform=linux_appimage_x86");
+	urlStr.append("&platform=linux_appimage");
 #elif BOOST_OS_WINDOWS
 	urlStr.append("&platform=windows");
 #elif BOOST_OS_MACOS
-	urlStr.append("&platform=macos_bundle_x86");
+	urlStr.append("&platform=macos_bundle");
 #elif
 #error Name for current platform is missing
 #endif
+#if defined(__aarch64__)
+	urlStr.append("_aarch64");
+#elif defined(ARCH_X86_64)
+	urlStr.append("_x86_64");
+#else
+	urlStr.append("_unknown");
+#endif
+
 	const auto& config = GetWxGUIConfig();
 	if(config.receive_untested_updates)
 		urlStr.append("&allowNewUpdates=1");
