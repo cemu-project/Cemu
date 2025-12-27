de12f9fb38458bba8c14369520257081634d6a8d
goeiecool9999 <7033575+goeiecool9999@users.noreply.github.com>
2025-12-27 14:40:22 +0100

    Remove duplicate WAW detection.

--- src/Cafe/HW/Latte/Renderer/Vulkan/VulkanRendererCore.cpp
+++ src/Cafe/HW/Latte/Renderer/Vulkan/VulkanRendererCore.cpp
@@ -1093,7 +1093,7 @@ void VulkanRenderer::sync_RenderPassLoadTextures(CachedFBOVk* fboVk)
 	{
 		LatteTextureVk* texVk = (LatteTextureVk*)tex;
 
-		//RAW
+		//RAW / WAW
 		if (texVk->m_vkFlushIndex_write == m_state.currentFlushIndex)
 			flushRequired = true;
 
@@ -1111,9 +1111,6 @@ void VulkanRenderer::sync_RenderPassStoreTextures(CachedFBOVk* fboVk)
 		LatteTextureVk* texVk = (LatteTextureVk*)tex;
 		//WAR
 		if (texVk->m_vkFlushIndex_read == m_state.currentFlushIndex)
-			flushRequired = true;
-		//WAW
-		if (texVk->m_vkFlushIndex_write == m_state.currentFlushIndex)
 			flushRequired = true;
 		texVk->m_vkFlushIndex_write = m_state.currentFlushIndex;
 	}
