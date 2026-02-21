f7f8c06935902e20d80fb16c09be15effe27b5d9
capitalistspz <keipitalists@proton.me>
2026-02-21 23:01:20 +0000

    Rebased

---
+++ src/gui/wxgui/CameraSettingsWindow.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <wx/dialog.h>
+#include <wx/timer.h>
+#include <wx/choice.h>
+#include <wx/bmpbuttn.h>
+
+class CameraSettingsWindow : public wxDialog
+{
+	wxChoice* m_cameraChoice;
+	wxButton* m_refreshButton;
+	wxWindow* m_imageWindow;
+	wxBitmap m_imageBitmap;
+	wxTimer m_imageUpdateTimer;
+	std::vector<uint8> m_imageBuffer;
+  public:
+	explicit CameraSettingsWindow(wxWindow* parent);
+	void OnSelectCameraChoice(wxCommandEvent&);
+	void OnRefreshPressed(wxCommandEvent&);
+	void UpdateImage(const wxTimerEvent&);
+	void OnClose(wxCloseEvent& event);
+};
