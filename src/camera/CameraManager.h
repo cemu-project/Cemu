c0b25b0745b8f095e7c6eb4e418eafa825d24906
capitalistspz <keipitalists@proton.me>
2026-02-21 23:01:20 +0000

    Rebased

---
+++ src/camera/CameraManager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#pragma once
+#include <string>
+
+namespace CameraManager
+{
+    struct DeviceInfo
+    {
+        std::string uniqueId;
+        std::string name;
+    };
+    constexpr static uint32 DEVICE_NONE = std::numeric_limits<uint32>::max();
+
+    void Init();
+    void Deinit();
+    void Open();
+    void Close();
+
+    void FillNV12Buffer(uint8* nv12Buffer);
+    void FillRGBBuffer(uint8* rgbBuffer);
+
+    void SetDevice(uint32 deviceNo);
+    std::vector<DeviceInfo> EnumerateDevices();
+    void SaveDevice();
+    std::optional<uint32> GetCurrentDevice();
+} // namespace CameraManager
