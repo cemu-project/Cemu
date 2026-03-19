c92bef0352d1ca894e5c40d250b14218c97893c2
Exzap <13877693+Exzap@users.noreply.github.com>
2026-03-19 20:49:02 +0100

    coreinit: Refactor OSScreen

--- src/Cafe/OS/libs/coreinit/coreinit_OSScreen.h
+++ src/Cafe/OS/libs/coreinit/coreinit_OSScreen.h
@@ -1,6 +1,18 @@
 #pragma once
 
+#define OSSCREEN_TV		(0)
+#define OSSCREEN_DRC	(1)
+
 namespace coreinit
 {
+	void OSScreenInit();
+	uint32 OSScreenGetBufferSizeEx(uint32 screenIndex);
+	void OSScreenSetBufferEx(uint32 screenIndex, uint32 buffer);
+	void OSScreenEnableEx(uint32 screenIndex, uint32 isEnabled);
+	void OSScreenClearBufferEx(uint32 screenIndex, uint32 color);
+	void OSScreenFlipBuffersEx(uint32 screenIndex);
+	void OSScreenPutPixelEx(uint32 screenIndex, sint32 x, sint32 y, uint32 color);
+	void OSScreenPutFontEx(uint32 screenIndex, sint32 x, sint32 y, const char* str);
+
 	void InitializeOSScreen();
 }
