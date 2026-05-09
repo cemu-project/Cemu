1d9bdd99c9a761852fad44cfe1c3851b44cac5ef
SSimco <37044560+SSimco@users.noreply.github.com>
2026-05-09 17:06:20 +0300

    input: add methods to override button and axis values

--- src/input/emulated/EmulatedController.cpp
+++ src/input/emulated/EmulatedController.cpp
@@ -264,6 +264,12 @@ void EmulatedController::clear_controllers()
 float EmulatedController::get_axis_value(uint64 mapping) const
 {
 	std::shared_lock lock(m_mutex);
+	const auto overriddenAxisMappingIt = m_overriddenAxisMappings.find(mapping);
+	if (overriddenAxisMappingIt != m_overriddenAxisMappings.cend() && overriddenAxisMappingIt->second != 0.0f)
+	{
+		return overriddenAxisMappingIt->second;
+	}
+
 	const auto it = m_mappings.find(mapping);
 	if (it != m_mappings.cend())
 	{
@@ -275,9 +281,25 @@ float EmulatedController::get_axis_value(uint64 mapping) const
 	return 0;
 }
 
+void EmulatedController::set_button_value(uint64 mapping, bool value)
+{
+	m_overriddenButtonMappings[mapping] = value;
+
+}
+void EmulatedController::set_axis_value(uint64 mapping, float value)
+{
+	m_overriddenAxisMappings[mapping] = value;
+}
+
 bool EmulatedController::is_mapping_down(uint64 mapping) const
 {
 	std::shared_lock lock(m_mutex);
+	const auto overriddenButtonMappingIt = m_overriddenButtonMappings.find(mapping);
+	if (overriddenButtonMappingIt != m_overriddenButtonMappings.cend() && overriddenButtonMappingIt->second)
+	{
+		return true;
+	}
+
 	const auto it = m_mappings.find(mapping);
 	if (it != m_mappings.cend())
 	{
