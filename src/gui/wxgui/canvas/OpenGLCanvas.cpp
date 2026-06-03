365fdc9a5df06364c8e8f4f0fb9fdc0b7822053c
codeman4033 <92231243+codeman4033@users.noreply.github.com>
2026-06-03 13:35:02 -0500

    Fix apparent error.
    Co-authored-by: goeiecool9999 <7033575+goeiecool9999@users.noreply.github.com>

--- src/gui/wxgui/canvas/OpenGLCanvas.cpp
+++ src/gui/wxgui/canvas/OpenGLCanvas.cpp
@@ -119,7 +119,7 @@ public:
 	void UpdateVSyncState()
 	{
 		int configValue = GetConfig().vsync.GetValue();
-		(value < 0) ? 0 : (value > 1) ? 1 : value;
+		configValue = configValue > 0 ? 1 : 0;
 		if(m_activeVSyncState != configValue)
 		{
 #if BOOST_OS_WINDOWS
